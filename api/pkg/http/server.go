ef66de9647fb687465fb9f33f9d397b0bce451c6
André Mendonça e Silva <dexmendoncadev@gmail.com>
2024-10-01 16:34:27 -0300

    feat: add favicon support

--- api/pkg/http/server.go
+++ api/pkg/http/server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 	"tgo/api/internal/di"
 	"tgo/api/internal/modules/http/routes"
 	"tgo/api/pkg/middleware"
@@ -17,9 +18,24 @@ func Setup() *fiber.App {
 	if err != nil {
 		log.Fatalf("File Version does not exist: %v", err)
 	}
+
 	app := fiber.New(fiber.Config{
 		Prefork: true,
 		AppName: fmt.Sprintf("TGo v%v", string(dat)),
+		ErrorHandler: func(c *fiber.Ctx, err error) error {
+
+			if fiberErr, ok := err.(*fiber.Error); ok && fiberErr.Code == fiber.StatusNotFound {
+				return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+					"error":   true,
+					"message": "Página não encontrada",
+				})
+			}
+
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error":   true,
+				"message": "Erro interno no servidor",
+			})
+		},
 	})
 
 	app.Use(middleware.CORS())
@@ -31,6 +47,10 @@ func Setup() *fiber.App {
 	}
 
 	routes.StartRoutes(app, container)
+
+	relativePath := "./api/assets/favicon.ico"
+	absolutePath, _ := filepath.Abs(relativePath)
+	app.Static("/favicon.ico", absolutePath)
 
 	return app
 }
